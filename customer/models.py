bd2eb39d40a4de641108128edf58a4522366fea2
Luan Fernandes <hello@luanfernandes.dev>
2020-11-06 18:54:48 -0300

    create customer classes -> Base,Customer

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract: True
+
+
+class Customer(Base):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    birth_date = models.DateField()
+    area_code = models.CharField(max_length=3)
+    phone_number = models.CharField(max_length=9)
+    country = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        db_table = 'customer'
