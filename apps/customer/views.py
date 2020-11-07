e8712972ee1be889ff64883c7b2263e91065488a
Luan Fernandes <hello@luanfernandes.dev>
2020-11-07 16:02:04 -0300

    Criação do formulário de ciente

--- apps/customer/views.py
+++ apps/customer/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Customer
+from .forms import CustomerForm
 
 
 # Create your views here.
@@ -8,3 +9,11 @@ class CustomerListView(ListView):
     template_name = 'customer/customer_list.html'
     model = Customer
     queryset = Customer.objects.all()
+
+
+class CustomerCreateView(CreateView):
+    template_name = 'customer/customer.html'
+    form_class = CustomerForm
+
+    def form_valid(self, form):
+        return super().form_valid(form)
