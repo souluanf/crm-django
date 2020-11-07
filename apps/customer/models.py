9445e937832e940c56181d135def57a1f04ec167
Luan Fernandes <hello@luanfernandes.dev>
2020-11-07 04:38:30 -0300

    customer list view

--- apps/customer/models.py
+++ apps/customer/models.py
@@ -15,14 +15,23 @@ class Customer(Base):
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     birth_date = models.DateField()
-    area_code = models.CharField(max_length=3)
+    area_code = models.CharField(max_length=2)
     phone_number = models.CharField(max_length=9)
     country = models.CharField(max_length=30)
-    state = models.CharField(max_length=30)
+    state = models.CharField(max_length=2)
     city = models.CharField(max_length=30)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+    def get_full_phone_number(self):
+        return f'({self.area_code}) {self.phone_number}'
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_full_city(self):
+        return f'{self.city} - {self.state}'
 
     class Meta:
         db_table = 'customer'
