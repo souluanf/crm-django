3454b970d292b82ba72ed7a26634a321899360a4
Luan Fernandes <hello@luanfernandes.dev>
2020-11-07 22:15:56 -0300

    Buscar por nome ou sobrenome

--- apps/customer/views.py
+++ apps/customer/views.py
@@ -6,6 +6,8 @@ from django.views.generic import (
 )
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
+
 from .models import Customer
 from .forms import CustomerForm
 
@@ -16,7 +18,16 @@ class CustomerListView(ListView):
     template_name = 'customer/customer_list.html'
     paginate_by = 2
     model = Customer
-    queryset = Customer.objects.all()
+
+    def get_queryset(self):
+        name = self.request.GET.get('name')
+        if name:
+            object_list = self.model.objects.filter(
+                Q(first_name__icontains=name) | Q(last_name__icontains=name)
+            )
+        else:
+            object_list = self.model.objects.all()
+        return object_list
 
 
 class CustomerCreateView(CreateView):
