be0b91816a287debd97ce1b9aa49feec02e1ee78
Luan Fernandes <hello@luanfernandes.dev>
2020-11-08 00:16:10 -0300

    logout

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login as app_login
+from django.contrib.auth import (
+    authenticate,
+    login as app_login,
+    logout as app_logout
+)
 from django.contrib import messages
-
-
-# Create your views here.
 
 
 def login(request):
@@ -20,5 +21,9 @@ def submit_login(request):
             return redirect('index')
         else:
             messages.error(request, 'Usuário/Senha inválido. Por favor tente novamente.')
+    return redirect('login')
 
+
+def logout(request):
+    app_logout(request)
     return redirect('login')
